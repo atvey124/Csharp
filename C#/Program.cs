168c5b47c775c095da00f3474f640596862bd8c9
atvey124 <matveygghhgg@gmail.com>
2024-01-11 19:12:16 +0300

    get pull

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿﻿// типы данных в c#
+
+
+int integer = 1245; // цело численные
+bool boolian = false; // логические значения true false 
+char character = '!'; // знаки,пишутся внутри ''
+double double = 5.55; // дробные 
+string string = "Hi"; // строки
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(e);
+Console.WriteLine(h);
+
+
+// бинарные операции 
+
+
+int a = 12;
+int b = 13;
+int c = a + b; //новая переменная будет иметь тип данных 'int',поскольку в бинарной операции два операнда имеют одинокавый тип данных
+Console.WriteLine(c);
