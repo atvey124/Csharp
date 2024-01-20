7fa9cdfb3a8da41d0e376f471e8e183309371b39
atvey124 <matveygghhgg@gmail.com>
2024-01-20 22:51:47 +0300

    remake basic game on rule void on branch draft

--- Csharp/C#/Program.cs
+++ Csharp/C#/Program.cs
@@ -734,72 +734,109 @@ int random3 = random2.Next(1,1001);
 int attempt = 10;
 int attempt1 = 10;
 int win = 0;
-int remaining_attempt = 0;
-int remaining_attempt1 = 0;
+int remaining_attempt = 1;
+int remaining_attempt1 = 1;
 while(attempt != 0)
-{   
-    Console.Write($"У тебя осталось {attempt} попыток, ");
-    Console.Write("Введите загаданное число: ");
+{ 
+    if (attempt > 4)
+    {
+        Console.Write($"У тебя осталось {attempt} попыток, ");
+        Console.Write("Введи загаданное число от 1 до 100: ");
+    } 
+    else if ( attempt <= 4)
+    {
+        Console.Write($"У тебя осталось {attempt} попытки, ");
+        Console.Write("Введи загаданное число от 1 до 100: ");
+
+    }
     
     int num_people = int.Parse(Console.ReadLine()!);
-    if (random1 != num_people)
+    if (num_people <= 100)
     {
-        if ( random1 > num_people)
-        {
-            Console.WriteLine("Загаданое число больше");
-            attempt--;
-            remaining_attempt++;
-        }
-        else if ( random1 < num_people)
-        {
-            Console.WriteLine("Загаданное число меньше");
-            attempt--;
-            remaining_attempt++;
-        }
 
+    
+        if (random1 != num_people)
+        {
+            if ( random1 > num_people)
+            {
+                Console.WriteLine("Загаданое число больше");
+                attempt--;
+                remaining_attempt++;
+            }
+            else if ( random1 < num_people)
+            {
+                Console.WriteLine("Загаданное число меньше");
+                attempt--;
+                remaining_attempt++;
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"Ты выиграл с {remaining_attempt} попытки ,загаданное число было {random1}!");
+            win++;
+            if (win == 1)
+            {  
+                Console.WriteLine();
+                Console.WriteLine("РАЗ УЖ ТЫ ВЫИГРАЛ,ПОПРОБУЙ ЭТО!!!");
+                while ( attempt1 != 0)
+                { 
+                    if (attempt1 > 4)
+                    {
+                        Console.Write($"У тебя осталось {attempt1} попыток, ");
+                        Console.Write("Введи загаданное число от 1 до 1000: ");
+                    }
+                    else if (attempt1 <= 4)
+                    {
+                        Console.Write($"У тебя осталось { attempt1} попытки, ");
+                        Console.Write("Введи загаданное число от 1 до 1000: ");
+                    }
+                    int num_people2 = int.Parse(Console.ReadLine()!);
+                    if (num_people2 <= 1000)
+                    {
+                    
+                              
+                        if (random3 != num_people2)
+                        {
+                            if ( random3 > num_people2)
+                            {
+                                Console.WriteLine("Загаданное число больше");
+                                attempt1--;
+                                remaining_attempt1++;
+                            }
+                            else if ( random3 < num_people2)
+                            {
+                                Console.WriteLine("Загаданное число меньше");
+                                attempt1--;
+                                remaining_attempt1++;
+                            }
+                        }
+                    else
+                    {
+                        Console.WriteLine($"Ты выиграл с {remaining_attempt1} попытки,загаданное число было {random3}! ");
+                        break;
+                    }
+                    }      
+                else
+                {
+                    Console.WriteLine("Ты ввел число больше чем диапазон угадывания ");
+                }
+                
+                    
+                
+                
+
+                }
+                Console.WriteLine($"Ты не смог угадать число от 1 до 1000, загаданное число было {random3}");
+                break;         
+
+            }
+        }
     }
     else
     {
-        Console.WriteLine($"Ты выиграл с {remaining_attempt} попытки ,загаданное число было {random1}!");
-        win++;
-        if (win == 1)
-        {   
-            Console.WriteLine();
-            Console.WriteLine("РАЗ УЖ ТЫ ВЫИГРАЛ,ПОПРОБУЙ ЭТО!!!");
-            while ( attempt1 != 0)
-            { 
-                Console.Write($"У тебя осталось {attempt1} попыток, ");
-                Console.Write("Введите загаданное число от 1 до 1000: ");
-                int num_people2 = int.Parse(Console.ReadLine()!);
-                if (random3 != num_people2)
-                {
-                    if ( random3 > num_people2)
-                    {
-                        Console.WriteLine("Загаданное число больше");
-                        attempt1--;
-                        remaining_attempt1++;
-                    }
-                    else if ( random3 < num_people2)
-                    {
-                        Console.WriteLine("Загаданное число меньше");
-                        attempt1--;
-                        remaining_attempt1++;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Ты выиграл с {remaining_attempt1} попытки,загаданное число было {random3}! ");
-                    break;
-                }
-
-                
-
-            }
-            
-
-        }
+        Console.WriteLine("Ты ввел число больше диапазона угадывания ");
     }
 }
-
 
 
